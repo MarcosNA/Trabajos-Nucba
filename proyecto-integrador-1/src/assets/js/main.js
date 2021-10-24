c539b9bb11d74c95253546a8a0bb5cc60e214d3d
MarcosNa <Nahuel_Marcos15@hotmail.com>
2021-10-24 12:20:17 -0300

    update landing page

--- proyecto-integrador-1/src/assets/js/main.js
+++ proyecto-integrador-1/src/assets/js/main.js
@@ -1,12 +1,19 @@
 addEventListener("DOMContentLoaded", () => {
-    const iconMenu = document.querySelector(".icon-menu")
+    const iconMenu = document.querySelector(".btn-nav")
     if(iconMenu){
         iconMenu.addEventListener("click", () => {
-            const nav = document.querySelector("navbar")
+            const nav = document.querySelector(".navbar")
             nav.classList.toggle("show")
-            const login = document.querySelector("login")
-            login.classList.toggle("show")
-
+           
         })
     }
+
+    const a = document.querySelectorAll(".a-nav")
+    console.log(a)
+    for (let i = 0 ; i < a.length; i++) {
+        a[i].addEventListener('click',() => {
+            const nav = document.querySelector(".navbar")
+            nav.classList.toggle("show")
+        } ) ; 
+     }   
 })
